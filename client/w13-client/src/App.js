15c03cb7a4d6728a95f625d6eb8d1a9783e20368
niper <w.nicolasperdomo@gmail.com>
2022-09-12 18:28:23 -0400

    Navbar removed

--- client/w13-client/src/App.js
+++ client/w13-client/src/App.js
@@ -1,53 +1,62 @@
-import "./App.css"
-import Navbar from "./components/Navbar";
-import Playlists from './components/Playlists'
-import DiscorgsContent from './components/DiscorgsContent'
+import "./App.css";
+import Playlists from "./components/Playlists";
+import DiscorgsContent from "./components/DiscorgsContent";
 import React from "react";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        //Setting initial state
-        this.state = {
-            savedPlaylists: [],
-            loading: true,
-        };
-    }
+    //Setting initial state
+    this.state = {
+      savedPlaylists: [],
+      loading: true,
+    };
+  }
 
-    getSavedPlaylists = async()=>{
-        let url = "http://localhost:8000/playlists"
-        let response = await fetch(url);
-        let json = await response.json();
-        this.setState({savedPlaylists:json, loading:false})
-    }
+  getSavedPlaylists = async () => {
+    let url = "http://localhost:8000/playlists";
+    let response = await fetch(url);
+    let json = await response.json();
+    this.setState({ savedPlaylists: json, loading: false });
+  };
 
-    componentDidMount(){
-        this.getSavedPlaylists();
-    }
-    
-   render(){
+  componentDidMount() {
+    this.getSavedPlaylists();
+  }
+
+  render() {
     return (
-    <div className="App">
+      <div className="App">
         <div className="container">
-            <div className="row">
-                <div id="navbar" className="col-2">
-                    <Navbar/>
-                    <div className='col'>
-                        <Playlists savedPlaylists={this.state.savedPlaylists}/>
-                    </div>
-                </div>
-                <div className="col" style={{backgroundColor:"#1c1c1c", minHeight:"100vh", color:'white'}}>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className={'col DiscorgsContent'}><DiscorgsContent savedPlaylists={this.state.savedPlaylists}/></div>
-                        </div>
-                    </div>
-                </div>
+          <div className="row">
+            <div id="navbar" className="col-2">
+              <div className="col">
+                <Playlists savedPlaylists={this.state.savedPlaylists} />
+              </div>
             </div>
+            <div
+              className="col"
+              style={{
+                backgroundColor: "#1c1c1c",
+                minHeight: "100vh",
+                color: "white",
+              }}
+            >
+              <div className="container">
+                <div className="row">
+                  <div className={"col DiscorgsContent"}>
+                    <DiscorgsContent
+                      savedPlaylists={this.state.savedPlaylists}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
--- client/w13-client/src/components/Playlists.js
+++ client/w13-client/src/components/Playlists.js
@@ -1,63 +1,72 @@
-import React from 'react';
+import React from "react";
 
 class Playlist extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        //Setting initial state
-        this.state = {
-            isLoading: true,
-            dataJSX:(<div className='row'>Loading ...</div>),
-            statusMessage:[]
-        };
-    }
+    //Setting initial state
+    this.state = {
+      isLoading: true,
+      dataJSX: <div className="row">Loading ...</div>,
+      statusMessage: [],
+    };
+  }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.savedPlaylists !== prevState.savedPlaylists) {
-            return ({ savedPlaylists: nextProps.savedPlaylists })
-        }
-        return null
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.savedPlaylists !== prevState.savedPlaylists) {
+      return { savedPlaylists: nextProps.savedPlaylists };
     }
+    return null;
+  }
 
-    componentDidUpdate(prevProps, prevState){
-        if((prevState.savedPlaylists !== this.state.savedPlaylists)){
-            this.updateDataJSX()
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.savedPlaylists !== this.state.savedPlaylists) {
+      this.updateDataJSX();
     }
+  }
 
-    handleOnClickRemovePlaylist = async(event)=>{
-        let playlistId = event.currentTarget.children.item(0).value
-        let url = 'http://localhost:8000/playlists/' + playlistId;
-        let response = await fetch(url, {
-                    method : 'DELETE',
-                    mode : 'cors',
-                });
-        let json = await response.json();
-        console.log(json)
-    }
+  handleOnClickRemovePlaylist = async (event) => {
+    let playlistId = event.currentTarget.children.item(0).value;
+    let url = "http://localhost:8000/playlists/" + playlistId;
+    let response = await fetch(url, {
+      method: "DELETE",
+      mode: "cors",
+    });
+    let json = await response.json();
+    console.log(json);
+  };
 
-    updateDataJSX(){
-        let jsx = [];
-        this.state.savedPlaylists.map((playlist)=>{
-            jsx.push(<div className='row' key={playlist.name + playlist.id}>
-                    <div className='col-10'>{playlist.name}</div>
-                    <div className='col-2'><button onClick={(event)=>{this.handleOnClickRemovePlaylist(event)}} style={{color:'white', border:'none', background:'none'}}><input type='hidden' name='id' value={playlist.id}/>x</button></div>
-                </div>)
-            return true
-        })
-        this.setState({dataJSX:jsx})
-    }
+  updateDataJSX() {
+    let jsx = [];
+    this.state.savedPlaylists.map((playlist) => {
+      jsx.push(
+        <div className="row" key={playlist.name + playlist.id}>
+          <div className="col-10">{playlist.name}</div>
+          <div className="col-2">
+            <button
+              onClick={(event) => {
+                this.handleOnClickRemovePlaylist(event);
+              }}
+              style={{ color: "white", border: "none", background: "none" }}
+            >
+              <input type="hidden" name="id" value={playlist.id} />x
+            </button>
+          </div>
+        </div>
+      );
+      return true;
+    });
+    this.setState({ dataJSX: jsx });
+  }
 
-    render() {
-        return (
-            <div style={{color:'white'}}>
-                <h1>My Playlist</h1>
-                <div className='container'>
-                    {this.state.dataJSX}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ color: "white" }}>
+        <h3 style={{ margin: "5% 0 5% 0" }}>My Playlist</h3>
+        <div className="container">{this.state.dataJSX}</div>
+      </div>
+    );
+  }
 }
 
 export default Playlist;
